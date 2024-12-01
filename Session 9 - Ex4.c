a876c013df135d632bb535c281838b346137b45d
tientda1 <110662513+tientda1@users.noreply.github.com>
2024-12-02 02:14:27 +0700

    Phùng Quang Tiến

--- Session 9 - Ex4.c
+++ Session 9 - Ex4.c
@@ -1,82 +1,5 @@
 #include <stdio.h>
 
-int Iarray(int arr[], int size) {
-    int i;
-    printf("Nhap so phan tu cua mang: ");
-    scanf("%d", &size);
-    for (i = 0; i < size; i++) {
-        printf("Nhap phan tu thu %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-    return size;
-}
-
-int Arr(int arr[], int size) {
-    int i;
-    printf("Mang hien tai: ");
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 0;
-}
-
-int input(int arr[], int size) {
-    int value, index;
-    if (size >= 100) {
-        printf("Mang da day, khong the them phan tu moi!\n");
-        return size;
-    }
-    printf("Nhap gia tri can them: ");
-    scanf("%d", &value);
-    printf("Nhap vi tri can them (tu 0 den %d): ", size);
-    scanf("%d", &index);
-
-    if (index < 0 || index > size) {
-        printf("Vi tri khong hop le!\n");
-        return size;
-    }
-
-    for (int i = size; i > index; i--) {
-        arr[i] = arr[i - 1];
-    }
-    arr[index] = value;
-    size++;
-    return size;
-}
-int edit(int arr[], int size) {
-    int index, value;
-    printf("Nhap vi tri can sua (tu 0 den %d): ", size - 1);
-    scanf("%d", &index);
-
-    if (index < 0 || index >= size) {
-        printf("Vi tri khong hop le!\n");
-        return -1;
-    }
-
-    printf("Nhap gia tri moi: ");
-    scanf("%d", &value);
-    arr[index] = value;
-    return 0;
-}
-
-int removeArr(int arr[], int size) {
-    int index;
-    printf("Nhap vi tri can xoa (tu 0 den %d): ", size - 1);
-    scanf("%d", &index);
-
-    if (index < 0 || index >= size) {
-        printf("Vi tri khong hop le!\n");
-        return size;
-    }
-
-    for (int i = index; i < size - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-    size--;
-    return size;
-}
-
 int main() {
     int arr[100], size = 0, choice;
 
@@ -92,15 +15,63 @@ int main() {
         scanf("%d", &choice);
 
         if (choice == 1) {
-            size = Iarray(arr, size);
+            printf("Nhap so phan tu cua mang: ");
+            scanf("%d", &size);
+            for (int i = 0; i < size; i++) {
+                printf("Nhap phan tu thu %d: ", i + 1);
+                scanf("%d", &arr[i]);
+            }
         } else if (choice == 2) {
-            Arr(arr, size);
+            printf("Mang hien tai: ");
+            for (int i = 0; i < size; i++) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
         } else if (choice == 3) {
-            size = input(arr, size);
+            if (size >= 100) {
+                printf("Mang da day, khong the them phan tu moi!\n");
+            } else {
+                int value, index;
+                printf("Nhap gia tri can them: ");
+                scanf("%d", &value);
+                printf("Nhap vi tri can them (tu 0 den %d): ", size);
+                scanf("%d", &index);
+
+                if (index < 0 || index > size) {
+                    printf("Vi tri khong hop le!\n");
+                } else {
+                    for (int i = size; i > index; i--) {
+                        arr[i] = arr[i - 1];
+                    }
+                    arr[index] = value;
+                    size++;
+                }
+            }
         } else if (choice == 4) {
-            edit(arr, size);
+            int index, value;
+            printf("Nhap vi tri can sua (tu 0 den %d): ", size - 1);
+            scanf("%d", &index);
+
+            if (index < 0 || index >= size) {
+                printf("Vi tri khong hop le!\n");
+            } else {
+                printf("Nhap gia tri moi: ");
+                scanf("%d", &value);
+                arr[index] = value;
+            }
         } else if (choice == 5) {
-            size = removeArr(arr, size);
+            int index;
+            printf("Nhap vi tri can xoa (tu 0 den %d): ", size - 1);
+            scanf("%d", &index);
+
+            if (index < 0 || index >= size) {
+                printf("Vi tri khong hop le!\n");
+            } else {
+                for (int i = index; i < size - 1; i++) {
+                    arr[i] = arr[i + 1];
+                }
+                size--;
+            }
         } else if (choice == 6) {
             printf("Thoat chuong trinh.\n");
         } else {
